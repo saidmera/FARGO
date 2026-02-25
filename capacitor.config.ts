f2a6209e838365454ec5846b73dd61d57e405a59
saidmera <55964968+saidmera@users.noreply.github.com>
2026-02-25 23:35:38 +0100

    Add initial Capacitor configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cargoswift.app',
+  appName: 'CargoSwift',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
